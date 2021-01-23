5d2636cd0fa968afa578376af049c4b671ac5128
Panche <panchemarkoski@gmail.com>
2021-01-23 15:45:16 +0100

    Insert Modules CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const pageOne = () => {
+  return(
+    <div>
+      pageOne
+      <Link to="/pagetwo">Forward</Link>
     </div>
-  );
+  )
 }
 
-export default App;
+const pageTwo = () => {
+  return(
+    <div>
+      pageTwo
+      <Link to="/">back</Link>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={pageOne}/>
+        <Route path="/pagetwo" component={pageTwo}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
